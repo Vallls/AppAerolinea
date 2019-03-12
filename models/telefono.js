6dc7916008d39242c990c4d2c767264e4ea43534
Valeria Madio <valeria.madio@correo.unimet.edu.ve>
2019-03-11 20:02:42 -0400

    telefono and user

--- models/telefono.js
+++ models/telefono.js
@@ -4,7 +4,7 @@ const user = require('./user');
 
 const telefono = db.define('Telefono', {
     telefono: {
-        type: sequelize.INTEGER,
+        type: sequelize.DOUBLE,
         allowNull: false,
         validate: {
             notEmpty: true,
