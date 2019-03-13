ae4bb564179bedcae8dc6572acf3659e384b6cb4
Mary Beltrán <m.beltran@correo.unimet.edu.ve>
2019-03-13 17:33:02 -0400

    getUsuario en Reserva Vuelo

--- controllers/vueloC.js
+++ controllers/vueloC.js
@@ -5,19 +5,22 @@ const vuelo = require('../models/vuelo');
 
 const controller = {};
 
+
 controller.getVuelos = function(callback){
-    
+        var x;
         db.query(
             "SELECT CONCAT(`Rutas`.`idruta`, ' ',`Rutas`.`origenIATA`, ' ',`Rutas`.`destinoIATA`, ' ',`Rutas`.`horsalidp`, ' ',`Vuelo`.`fechavuelo`) AS `Datos`" + 
             "FROM `Rutas`" +
             "INNER JOIN `Vuelo` ON `Rutas`.`idruta` = `Vuelo`.`RutaIdruta`"
         ).spread((data,metada) => {
-            if (data) {
-                callback(data, null);
-              } else {
-                callback(null, new Error('Uppps... Hemos tenido error en nuestra Base de datos'));
-              }
+            x = data;
         });
+        if (x) {
+          callback(x, null);
+        } else {
+          callback(null, new Error('Uppps... Hemos tenido error en nuestra Base de datos'));
+        }
+        callback(x,null);
 }
 
 
